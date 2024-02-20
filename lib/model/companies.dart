82c4669294d5968ca72f7869a2c2b50289694393
JeffHenriqueM <jeffhenrique@outlook.com>
2024-02-20 10:39:49 -0400

    Adicionando model

---
+++ lib/model/companies.dart
@@ -0,0 +1,28 @@
+class companies {
+  final int idHost;
+  final String hostname;
+  final String descricao;
+  final double so;
+  final String iplan;
+  final String ipwan;
+  final String linkWAN;
+  final int cpu;
+  final int ram;
+  final double drive;
+  final String licenca;
+  final String antivirus;
+
+  companies(
+      this.idHost,
+      this.hostname,
+      this.descricao,
+      this.so,
+      this.iplan,
+      this.ipwan,
+      this.linkWAN,
+      this.cpu,
+      this.ram,
+      this.drive,
+      this.licenca,
+      this.antivirus);
+}
